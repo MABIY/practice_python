7b3a3a4b7fe74f3fc6c3754eca69a154ceb06b15
liu.hua <liu.hua@tineco.com>
2025-01-16 22:07:33 +0800

    ..

---
+++ README.md
@@ -0,0 +1,3 @@
+# practice_python
+## python version
+Python 3.13.0
---
+++ chapter02/2-3.py
@@ -0,0 +1,3 @@
+name = "zoctopus"
+print("Hello, " + name + " " + "would you like to learn some Python today?")
+print(f"Hello, {name} would you like to learn some Python today?")
