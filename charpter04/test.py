345b7dda3e0c44c37040381e267b7afe85df373e
liu.hua <liu.hua@tineco.com>
2025-01-30 23:30:00 +0800

    ..

---
+++ charpter04/test.py
@@ -0,0 +1,12 @@
+my_foods =['pizza','falafel','carrot cake']
+# 这是行不通的
+friend_foods = my_foods
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favorite foods are:")
+print(my_foods)
+
+print("\nMy friend's favorite foods are:")
+print(friend_foods)
