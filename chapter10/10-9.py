d5be4f48355dc7ff450462a2c18bdb6d42c12ad5
liu.hua <liu.hua@tineco.com>
2025-02-09 19:07:45 +0800

    ..

---
+++ chapter10/10-9.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+filenames = ['cats.txt','dogs.txt']
+for filename in filenames:
+    print(f"\nReading file: {filename}")
+
+    path = Path(filename)
+    try:
+        contents = path.read_text()
+    except FileNotFoundError:
+        pass
+    else:
+        print(contents)
--- chapter10/test.py
+++ chapter10/test.py
@@ -1,13 +1,8 @@
 from pathlib import Path
 
-path =Path('alice.txt')
-try:
-    contents = path.read_text(encoding='utf-8')
-except FileNotFoundError:
-    print(f"Sorry, the file {path} does not exist.")
-else:
-    # 计算文件大致包含多少个单词
-    words = contents.split()
-    print(words)
-    num_words = len(words)
-    print(f"The file {path} has about {num_words} words.")
+from word_count import count_worlds
+
+filenames = ['alice.txt','siddhartha.txt','moby_dick.txt','little_women.txt']
+for filename in filenames:
+    path = Path(filename)
+    count_worlds(path)
